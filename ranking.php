f0f1595716535a82db0576e1554157da2a5c0c35
jc91715 <370234915@qq.com>
2019-04-29 11:29:17 +0800

    Update ranking.php

--- ranking.php
+++ ranking.php
@@ -18,7 +18,14 @@ $arr = [
     ]
 ];
 array_multisort(array_column($arr,'score'),SORT_ASC,$arr);
-$results = array_slice($arr, 0, 3, true);
+$n=3;
+$int =3;
+$results = array_slice($arr, 0, $n, true);
+
+//前几名是否重复
+while (count(array_count_values(array_column($results,'score')))<$n&&(count($results)<count($arr))){
+    $results = array_slice($arr, 0, $int++, true);
+}
 $length = count($arr);
 $resultLength = count($results);
 while($resultLength<$length &&$results[$resultLength-1]['score']==$arr[$resultLength]['score']){
