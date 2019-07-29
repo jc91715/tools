88e12daa289708595302bbaaa4bd9c9ae84f5d3f
jc91715 <370234915@qq.com>
2019-07-29 21:11:31 +0800

    Update maze.php

--- maze.php
+++ maze.php
@@ -1,6 +1,6 @@
 <?php
 
-$n=19;
+$n=300;
 $positions=[];
 for ($i=0;$i<=$n;$i++){
     for ($j=0;$j<=$n;$j++){
@@ -13,44 +13,44 @@ $startDot=[0,0];
 //已经放置的点
 $placedDot=[];
 //不可放置的点
-$notPlacedDot=notPlacedDot($positions);
+$notPlacedDot=notPlacedDot($positions,$n);
 $placedDot[]=$startDot;
 
 
 
 //所有穿过的点
-$lineDot=line($positions,$startDot,$placedDot,$notPlacedDot);
+$lineDot=line($positions,$startDot,$placedDot,$notPlacedDot,$n);
 
-echo getHtml($positions,$lineDot);
+echo getHtml($positions,$lineDot,$n);
 
 
-function line($positions,$startDot,$placedDot,$notPlacedDot){
+function line($positions,$startDot,$placedDot,$notPlacedDot,$n){
     
     $dotWrapper=GetDotWrappers($startDot);
     $key=rand(0,3);
     $nextDot =  $dotWrapper[$key];
 
     $i=0;
-    while (in_array($nextDot,$placedDot)||in_array($nextDot,$notPlacedDot)){
+    $notPlacedDotAll =array_merge($notPlacedDot,[]);
+    while (in_array($nextDot,$placedDot)||in_array($nextDot,$notPlacedDotAll)){
 
         $i++;
-       if(badDot($nextDot,$placedDot,$notPlacedDot)){//是否是坏点
+       if(badDot($nextDot,$placedDot,$notPlacedDot,$n)){//是否是坏点
 
-           $notPlacedDot[]=$nextDot;
+           $notPlacedDotAll[]=$nextDot;
 
           $startDot=$placedDot[count($placedDot)-1];
            $dotWrapper=GetDotWrappers($startDot);
            $key=rand(0,3);
            $nextDot =  $dotWrapper[$key];
 
-          if(badDot($startDot,$placedDot,$notPlacedDot)){//本身是否是坏点
-              $notPlacedDot[]= array_pop($placedDot);
+          if(badDot($startDot,$placedDot,$notPlacedDotAll,$n)){//本身是否是坏点
+              $notPlacedDotAll[]= array_pop($placedDot);
 
               $startDot=$placedDot[count($placedDot)-1];
               $dotWrapper=GetDotWrappers($startDot);
               $key=rand(0,3);
               $nextDot =  $dotWrapper[$key];
-
 
           }
 
@@ -59,8 +59,8 @@ function line($positions,$startDot,$placedDot,$notPlacedDot){
             $nextDot =  $dotWrapper[$key];
 
         }
-        if($i>3&&count($placedDot)>3){//循环大于3次 还没找到去除该点
-
+        if($i>4&&count($placedDot)>1){//循环大于3次 还没找到去除该点
+            $i=0;
             $notPlacedDot[]= array_pop($placedDot);
             $startDot=$placedDot[count($placedDot)-1];
             $dotWrapper=GetDotWrappers($startDot);
@@ -74,22 +74,21 @@ function line($positions,$startDot,$placedDot,$notPlacedDot){
 
 
     $placedDot[]=$nextDot;
-    $notPlacedDot=notPlacedDot($positions);
-//    print_r($nextDot);
+
    if($nextDot[0]>=19&&$nextDot[1]>0&&$nextDot[1]<=19){
        return $placedDot;
    }
-   return line($positions,$nextDot,$placedDot,$notPlacedDot);
+   return line($positions,$nextDot,$placedDot,$notPlacedDot,$n);
 }
 
-function getHtml($positions,$lineDot){
-    $str="<table  border='1' style='width:100%;text-align:center'>";
+function getHtml($positions,$lineDot,$n){
+    $str="<table border='1'>";
 
-    foreach (array_chunk($positions,20) as $row){
+    foreach (array_chunk($positions,$n+1) as $row){
         $str.="<tr>";
         foreach ($row as $tr){
             if(in_array($tr,$lineDot)){
-                $str.="<td style='color: white;background-color:red;'>{$tr[0]},{$tr[1]}</td>";
+                $str.="<td style='color: white;background-color: red'>点{$tr[0]},{$tr[1]}</td>";
             }else{
                 $str.="<td>{$tr[0]},{$tr[1]}</td>";
             }
@@ -101,7 +100,7 @@ function getHtml($positions,$lineDot){
     return $str;
 }
 
-function notPlacedDot($positions)
+function notPlacedDot($positions,$n)
 {
     
     $notPlacedDot=[];
@@ -109,7 +108,7 @@ function notPlacedDot($positions)
         $dotWrapper=GetDotWrappers($position);
 
         foreach ($dotWrapper as $dot){
-            if($dot[0]<0||$dot[1]<0||$dot[0]>19||$dot[1]>19){
+            if($dot[0]<0||$dot[1]<0||$dot[0]>$n||$dot[1]>$n){
                 $notPlacedDot[]=$dot;
             }
         }
@@ -124,12 +123,12 @@ function notPlacedDot($positions)
  * @param $notPlacedDot
  * @return bool
  */
-function badDot($position,$placedDot,$notPlacedDot){
+function badDot($position,$placedDot,$notPlacedDot,$n){
     
     $dotWrapper=GetDotWrappers($position);
     $i=0;
     foreach ($dotWrapper as $dot){
-        if($dot[0]<0||$dot[1]<0||$dot[1]>19||in_array($dot,$placedDot)||in_array($dot,$notPlacedDot)){
+        if($dot[0]<0||$dot[1]<0||$dot[1]>$n||in_array($dot,$placedDot)||in_array($dot,$notPlacedDot)){
             $i++;
         }
     }
@@ -184,3 +183,5 @@ function calculateXY($x,$y,$default='top')
 
     return [$x,$y];
 }
+
+
